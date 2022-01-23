4c982e4951f9d0bbd7da6c0639622aa8a2c3f974
Adeyinka Giwa <yinkadev1992@gmail.com>
2022-01-23 16:15:42 +0000

    launched on aws ec2  instance

---
+++ public/foodlist.js
@@ -0,0 +1,68 @@
+const foods = [
+    {
+        'name': 'Tropical Delight',
+        'price': 19.99,
+        'image': 17,
+        'description': 'Low calorie snack made with cheese, bread and fresh meat'
+    },
+    {
+        'name': 'Tropical Delight',
+        'price': 12.99,
+        'image': 1,
+        'description': 'Low calorie snack made with cheese, bread and fresh meat'
+    },
+    {
+        'name': 'Tropical Delight',
+        'price': 12.99,
+        'image': 10,
+        'description': 'Low calorie snack made with cheese, bread and fresh meat'
+    },
+    {
+        'name': 'Tropical Delight',
+        'price': 14.99,
+        'image': 9,
+        'description': 'Low calorie snack made with cheese, bread and fresh meat'
+    },
+    {
+        'name': 'Tropical Delight',
+        'price': 15.99,
+        'image': 8,
+        'description': 'Low calorie snack made with cheese, bread and fresh meat'
+    },
+    {
+        'name': 'Tropical Delight',
+        'price': 10.99,
+        'image': 7,
+        'description': 'Low calorie snack made with cheese, bread and fresh meat'
+    },
+    {
+        'name': 'Tropical Delight',
+        'price': 11.99,
+        'image': 6,
+        'description': 'Low calorie snack made with cheese, bread and fresh meat'
+    },
+    {
+        'name': 'Tropical Delight',
+        'price': 15.99,
+        'image': 5,
+        'description': 'Low calorie snack made with cheese, bread and fresh meat'
+    },
+    {
+        'name': 'Tropical Delight',
+        'price': 12.99,
+        'image': 4,
+        'description': 'Low calorie snack made with cheese, bread and fresh meat'
+    },
+    {
+        'name': 'Tropical Delight',
+        'price': 11.99,
+        'image': 3,
+        'description': 'Low calorie snack made with cheese, bread and fresh meat'
+    },
+    {
+        'name': 'Tropical Delight',
+        'price': 10.99,
+        'image': 2,
+        'description': 'Low calorie snack made with cheese, bread and fresh meat'
+    }
+];
--- resources/js/components/Main.js
+++ resources/js/components/Main.js
@@ -17,14 +17,83 @@ import Header from "./Header";
 import Home from "../views/Home";
 import Menu from "../views/Menu";
 
+const foods = [
+    {
+        'name': 'Tropical Delight',
+        'price': 19.99,
+        'image': 17,
+        'description': 'Low calorie snack made with cheese, bread and fresh meat'
+    },
+    {
+        'name': 'Tropical Delight',
+        'price': 12.99,
+        'image': 1,
+        'description': 'Low calorie snack made with cheese, bread and fresh meat'
+    },
+    {
+        'name': 'Tropical Delight',
+        'price': 12.99,
+        'image': 10,
+        'description': 'Low calorie snack made with cheese, bread and fresh meat'
+    },
+    {
+        'name': 'Tropical Delight',
+        'price': 14.99,
+        'image': 9,
+        'description': 'Low calorie snack made with cheese, bread and fresh meat'
+    },
+    {
+        'name': 'Tropical Delight',
+        'price': 15.99,
+        'image': 8,
+        'description': 'Low calorie snack made with cheese, bread and fresh meat'
+    },
+    {
+        'name': 'Tropical Delight',
+        'price': 10.99,
+        'image': 7,
+        'description': 'Low calorie snack made with cheese, bread and fresh meat'
+    },
+    {
+        'name': 'Tropical Delight',
+        'price': 11.99,
+        'image': 6,
+        'description': 'Low calorie snack made with cheese, bread and fresh meat'
+    },
+    {
+        'name': 'Tropical Delight',
+        'price': 15.99,
+        'image': 5,
+        'description': 'Low calorie snack made with cheese, bread and fresh meat'
+    },
+    {
+        'name': 'Tropical Delight',
+        'price': 12.99,
+        'image': 4,
+        'description': 'Low calorie snack made with cheese, bread and fresh meat'
+    },
+    {
+        'name': 'Tropical Delight',
+        'price': 11.99,
+        'image': 3,
+        'description': 'Low calorie snack made with cheese, bread and fresh meat'
+    },
+    {
+        'name': 'Tropical Delight',
+        'price': 10.99,
+        'image': 2,
+        'description': 'Low calorie snack made with cheese, bread and fresh meat'
+    }
+];
+
+
 function Main() {
 
     let [items, setItems] =  useState([])
 
     useEffect(()=>{
-           axios.get('/api/food').then(res=>{
-               setItems([...res.data])
-           })
+               setItems([...foods])
+
 
     },[]);
 
