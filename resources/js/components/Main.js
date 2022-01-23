ab2fc895e6dbb4245468be6bb89fea3dd796f160
Adeyinka Giwa <yinkadev1992@gmail.com>
2022-01-23 18:13:47 +0000

    fixing errors

--- resources/js/components/Main.js
+++ resources/js/components/Main.js
@@ -22,67 +22,79 @@ const foods = [
         'name': 'Tropical Delight',
         'price': 19.99,
         'image': 17,
-        'description': 'Low calorie snack made with cheese, bread and fresh meat'
+        'description': 'Low calorie snack made with cheese, bread and fresh meat',
+        'id': 1
     },
     {
         'name': 'Tropical Delight',
         'price': 12.99,
         'image': 1,
-        'description': 'Low calorie snack made with cheese, bread and fresh meat'
+        'description': 'Low calorie snack made with cheese, bread and fresh meat',
+        'id': 2
+
     },
     {
         'name': 'Tropical Delight',
         'price': 12.99,
         'image': 10,
-        'description': 'Low calorie snack made with cheese, bread and fresh meat'
+        'description': 'Low calorie snack made with cheese, bread and fresh meat',
+        'id': 3
     },
     {
         'name': 'Tropical Delight',
         'price': 14.99,
         'image': 9,
-        'description': 'Low calorie snack made with cheese, bread and fresh meat'
+        'description': 'Low calorie snack made with cheese, bread and fresh meat',
+        'id': 4
     },
     {
         'name': 'Tropical Delight',
         'price': 15.99,
         'image': 8,
-        'description': 'Low calorie snack made with cheese, bread and fresh meat'
+        'description': 'Low calorie snack made with cheese, bread and fresh meat',
+        'id': 5
     },
     {
         'name': 'Tropical Delight',
         'price': 10.99,
         'image': 7,
-        'description': 'Low calorie snack made with cheese, bread and fresh meat'
+        'description': 'Low calorie snack made with cheese, bread and fresh meat',
+        'id': 6
     },
     {
         'name': 'Tropical Delight',
         'price': 11.99,
         'image': 6,
-        'description': 'Low calorie snack made with cheese, bread and fresh meat'
+        'description': 'Low calorie snack made with cheese, bread and fresh meat',
+        'id': 7
     },
     {
         'name': 'Tropical Delight',
         'price': 15.99,
         'image': 5,
-        'description': 'Low calorie snack made with cheese, bread and fresh meat'
+        'description': 'Low calorie snack made with cheese, bread and fresh meat',
+        'id': 8
     },
     {
         'name': 'Tropical Delight',
         'price': 12.99,
         'image': 4,
-        'description': 'Low calorie snack made with cheese, bread and fresh meat'
+        'description': 'Low calorie snack made with cheese, bread and fresh meat',
+        'id': 12
     },
     {
         'name': 'Tropical Delight',
         'price': 11.99,
         'image': 3,
-        'description': 'Low calorie snack made with cheese, bread and fresh meat'
+        'description': 'Low calorie snack made with cheese, bread and fresh meat',
+        'id': 9
     },
     {
         'name': 'Tropical Delight',
         'price': 10.99,
         'image': 2,
-        'description': 'Low calorie snack made with cheese, bread and fresh meat'
+        'description': 'Low calorie snack made with cheese, bread and fresh meat',
+        'id': 10
     }
 ];
 
